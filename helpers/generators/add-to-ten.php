6ac16fd1c1db3ea171b7d18506dbdae6023dd173
forevermatt <forevermatt@users.noreply.github.com>
2016-11-30 20:26:20 -0500

    Change to unique, exhaustive set of examples.

--- helpers/generators/add-to-ten.php
+++ helpers/generators/add-to-ten.php
@@ -1,23 +1,12 @@
 <?php
-$prevNum1 = null;
-$prevNum2 = null;
-$prevYes = null;
-for ($i = 0; $i < 300; $i++) {
-  $num1 = mt_rand(1, 9);
-  $num2 = mt_rand(1, 9);
-  $yes = ($num1 + $num2 === 10) || mt_rand(0, 1);
-  if (($prevNum1 === $num1) && (($prevNum2 === $num2) || ($prevYes === $yes))) {
-    $i--;
-    continue;
+for ($num1 = 1; $num1 <= 9; $num1++) {
+  for ($num2 = 1; $num2 <= 9; $num2++) {
+    echo sprintf(
+      '      ["%s + %s", "%s"],%s',
+      $num1,
+      $num2,
+      (($num1 + $num2 === 10) ? 'Yes' : 'No'),
+      PHP_EOL
+    );
   }
-  echo sprintf(
-    '      ["%s + %s", "%s"],%s',
-    $num1,
-    ($yes ? (10 - $num1) : $num2),
-    ($yes ? 'Yes' : 'No'),
-    PHP_EOL
-  );
-  $prevNum1 = $num1;
-  $prevNum2 = $num2;
-  $prevYes = $yes;
 }
